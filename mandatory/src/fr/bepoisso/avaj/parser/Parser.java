cb74a4f8c68441b0f8f0699443488343907e50bb
bepoisso <github@bpoisson.com>
2026-06-30 21:21:43 +0200

    feat: add paser handling with custom throw

--- mandatory/src/fr/bepoisso/avaj/parser/Parser.java
+++ mandatory/src/fr/bepoisso/avaj/parser/Parser.java
@@ -1,13 +1,14 @@
 package fr.bepoisso.avaj.parser;
 
 import fr.bepoisso.avaj.exception.InvalidScenarioException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
 	private Path path = null;
-	private Files files = null;
 
 	public Parser(String p_filepath)
 	throws InvalidScenarioException {
@@ -16,34 +17,41 @@ public class Parser {
 			throw new InvalidScenarioException("Bad path");
 	}
 
-	private void checkFile()
+	private List<String[]> checkFile()
 	throws InvalidScenarioException {
-		// TODO add throw handling
-		List<String[]> result = null;
-		List<String> lines = Files.readAllLines(path);
+		List<String[]> result = new ArrayList<>();
+		try {
+			List<String> lines = Files.readAllLines(path);
 
-		for (int i = 0; i < lines.size(); ++i) {
-			String line = lines.get(i);
-			String[] tokens = line.split("\\s+");
-			if (i == 0) {
-				if (tokens.lenght != 1)
-					throw new InvalidScenarioException("Bad time simulation argument");
-				if (Integer.parseInt(tokens[0]) <= 0)
-					throw new InvalidScenarioException("Bad time simulation value");
+			for (int i = 0; i < lines.size(); ++i) {
+				String line = lines.get(i);
+				String[] tokens = line.split("\\s+");
+				if (i == 0) {
+					if (tokens.length != 1)
+						throw new InvalidScenarioException("Bad time simulation argument");
+					if (Integer.parseInt(tokens[0]) <= 0)
+						throw new InvalidScenarioException("Bad time simulation value");
+				}
+				else {
+					if (!tokens[0].equals("Baloon") && !tokens[0].equals("JetPlane") && !tokens[0].equals("Helicopter"))
+							throw new InvalidScenarioException("Unknown aircraft type");
+					if (!tokens[1].matches("[a-zA-Z0-9]+"))
+						throw new InvalidScenarioException("Bad name type");
+					if (tokens.length != 5)
+						throw new InvalidScenarioException("Bad number of arguments");
+					if (Integer.parseInt(tokens[2]) < 0)
+						throw new InvalidScenarioException("Bad longitude value");
+					if (Integer.parseInt(tokens[3]) < 0)
+						throw new InvalidScenarioException("Bad latitude value");
+					if (Integer.parseInt(tokens[4]) < 0)
+						throw new InvalidScenarioException("Bad height value");
+				}
+				result.add(tokens);
 			}
-			else {
-				if (tokens.lenght != 4)
-					throw new InvalidScenarioException("Bad number of arguments");
-				if (Integer.parseInt(tokens[2]) < 0)
-					throw new InvalidScenarioException("Bad longitude value");
-				if (Integer.parseInt(tokens[3]) < 0)
-					throw new InvalidScenarioException("Bad latitude value");
-				if (Integer.parseInt(tokens[4]) < 0)
-					throw new InvalidScenarioException("Bad height value");
-			}
-			line.add(tokens);
-			// TODO fix the add and return it
+		} catch (IOException e) {
+			throw new InvalidScenarioException(e.getMessage());
 		}
+		return result;
 	}
 
 	public void parse()
