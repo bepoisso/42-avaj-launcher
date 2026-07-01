0da677518d82a3269d728daec47738641560a11d
bpoisson <github@bpoisson.com>
2026-07-01 14:42:01 +0200

    clean: simplify factory methodes

--- mandatory/src/fr/bepoisso/avaj/aircraft/AircraftFactory.java
+++ mandatory/src/fr/bepoisso/avaj/aircraft/AircraftFactory.java
@@ -12,23 +12,23 @@ public class AircraftFactory {
 	public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates)
 	throws InvalidAircraftException {
 		++currentId;
-		Flyable result = null;
 
 		switch (p_type) {
-			case "Balloon" ->
-				result = new Balloon(currentId, p_name, p_coordinates);
-			case "JetPlane" ->
-				result = new JetPlane(currentId, p_name, p_coordinates);
-			case "Helicopter" ->
-				result = new Helicopter(currentId, p_name, p_coordinates);
+			case "Balloon" -> {
+				return new Balloon(currentId, p_name, p_coordinates);
+			}
+			case "JetPlane" -> {
+				return new JetPlane(currentId, p_name, p_coordinates);
+			}
+			case "Helicopter" -> {
+				return new Helicopter(currentId, p_name, p_coordinates);
+			}
 			default ->
 				throw new InvalidAircraftException("Bad aircraft type");
 		}
-		return result;
 	}
 
-	public static AircraftFactory getInstance()
-	throws InvalidAircraftException {
+	public static AircraftFactory getInstance() {
 		if (instance == null) {
 			instance = new AircraftFactory();
 		}
