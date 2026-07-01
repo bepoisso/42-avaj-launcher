2cebfa28a3864627bd6f2caf78eec859dc88503c
bpoisson <github@bpoisson.com>
2026-07-01 12:23:01 +0200

    feat: switch WeatherProvider class to singleton

--- mandatory/src/fr/bepoisso/avaj/weather/WeatherProvider.java
+++ mandatory/src/fr/bepoisso/avaj/weather/WeatherProvider.java
@@ -3,11 +3,19 @@ package fr.bepoisso.avaj.weather;
 import fr.bepoisso.avaj.coordinates.Coordinates;
 
 public class WeatherProvider {
+	private static WeatherProvider instance;
 	private String[] weather;
 
 	private WeatherProvider() {}
 
 	public String getCurrentWeather(Coordinates p_coordinates) {
 		// TODO
+	}
+
+	public static WeatherProvider getInstance() {
+		if (instance == null) {
+			instance = new WeatherProvider();
+		}
+		return instance;
 	}
 }
