472341dc666626aa4d702ecb73b4e522af3d51a4
bpoisson <github@bpoisson.com>
2026-07-01 12:21:53 +0200

    feat: switch AircraftFactory class to singleton

--- mandatory/src/fr/bepoisso/avaj/aircraft/AircraftFactory.java
+++ mandatory/src/fr/bepoisso/avaj/aircraft/AircraftFactory.java
@@ -4,8 +4,11 @@ import fr.bepoisso.avaj.coordinates.Coordinates;
 import fr.bepoisso.avaj.exception.InvalidAircraftException;
 
 public class AircraftFactory {
+	private static AircraftFactory instance;
 	private static int currentId = 0;
-	
+
+	private AircraftFactory() {}
+
 	public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates)
 	throws InvalidAircraftException {
 		++currentId;
@@ -22,5 +25,13 @@ public class AircraftFactory {
 				throw new InvalidAircraftException("Bad aircraft type");
 		}
 		return result;
+	}
+
+	public static AircraftFactory getInstance()
+	throws InvalidAircraftException {
+		if (instance == null) {
+			instance = new AircraftFactory();
+		}
+		return instance;
 	}
 }
