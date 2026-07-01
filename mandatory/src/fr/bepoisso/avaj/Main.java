fbf56184d98c54a1d39e2c5d4c9771ccc648367d
bpoisson <github@bpoisson.com>
2026-07-01 16:30:07 +0200

    feat: add main fonction with checker.

--- mandatory/src/fr/bepoisso/avaj/Main.java
+++ mandatory/src/fr/bepoisso/avaj/Main.java
@@ -1,7 +1,30 @@
 package fr.bepoisso.avaj;
 
+import fr.bepoisso.avaj.simulator.Simulator;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		if (args.length != 2) {
+			System.err.println("Wrong number of arguments");
+			return;
+		}
+
+		System.out.println("========================");
+		System.out.println("===== AVAJ-LUNCHER =====");
+		System.out.println("========================\n");
+		try {
+			Simulator simulator = new Simulator(args[1]);
+			simulator.parse();
+			System.out.println("Done ✅");
+			simulator.factory();
+			System.out.println("Done ✅");
+			simulator.run();
+			System.out.println("Done ✅");
+			System.out.println("\n--- End of simulation\n");
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			System.out.println("Fail ❌");
+		}
 	}
 }
