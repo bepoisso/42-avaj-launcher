40e95f1fb404d71fa7a45a0bfe7d94c64c9c76d0
bpoisson <github@bpoisson.com>
2026-07-01 16:18:14 +0200

    update: add throws to methode

--- mandatory/src/fr/bepoisso/avaj/weather/WeatherTower.java
+++ mandatory/src/fr/bepoisso/avaj/weather/WeatherTower.java
@@ -1,13 +1,16 @@
 package fr.bepoisso.avaj.weather;
 
-import fr.bepoisso.avaj.coordinates.Coordinates;
+import fr.bepoisso.avaj.exception.InvalidAircraftException;
+import fr.bepoisso.avaj.exception.InvalidLoggerException;
+import fr.bepoisso.avaj.simulator.Coordinates;
 
 public class WeatherTower extends Tower {
 	public String getWeather(Coordinates p_coordinates) {
 		return WeatherProvider.getInstance().getCurrentWeather(p_coordinates);
 	}
 
-	public void changeWeather() {
+	public void changeWeather()
+	throws InvalidAircraftException, InvalidLoggerException {
 		conditionChanged();
 	}
 }
